780e32aa1159a6c273033ad0eaf8363114fe1798
simon vadée <simonvadee@ip82-139-74-238.lijbrandt.net>
2016-10-02 17:59:33 +0200

    App skeleton (segues)

---
+++ Rolee/StartViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Rolee
+//
+//  Created by simon vadée on 01/10/2016.
+//  Copyright © 2016 simon vadée. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		print("start loaded")
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+	
+	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		print("before segue")
+	}
+}
+
