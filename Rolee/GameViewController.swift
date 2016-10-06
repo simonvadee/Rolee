93491461ef591ca6f2b1ef6c9d8d119d8dece9c8
Simon Vadée <simon.vadee@gmail.com>
2016-10-06 14:38:20 +0200

    hide navigation bar

--- Rolee/GameViewController.swift
+++ Rolee/GameViewController.swift
@@ -12,6 +12,7 @@ class GameViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		self.navigationController!.navigationBar.hidden = true;
 		// Do any additional setup after loading the view, typically from a nib.
 	}
 	
