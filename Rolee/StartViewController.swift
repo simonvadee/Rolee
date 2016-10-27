b3828c5d91e58691268d8dd8321e8b0f5935fdd8
IrisPoot <iris.poot@hotmail.com>
2016-10-27 21:17:32 +0200

    Finding the bug in retrieving of the iCloud username. Added a few 'prints' in the StartViewController

--- Rolee/StartViewController.swift
+++ Rolee/StartViewController.swift
@@ -27,6 +27,7 @@ class StartViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg-green.png")!)
+        print("Basic 1")
 
 		container.accountStatus() { [unowned self] status, error in
 			switch (status) {
@@ -47,7 +48,7 @@ class StartViewController: UIViewController {
 	}
 	
 	private func initUserInfoFromCloud() {
-	
+	print("Cloud 1")
 		container.fetchUserRecordID() { [unowned self] recordId, error in
 			if error == nil {
 				self.fetchUsername(recordId!)
@@ -57,10 +58,13 @@ class StartViewController: UIViewController {
 	}
 	
 	private func fetchUsername(_ recordId: CKRecordID) {
+        print("Username 1")
 		publicDB.fetch(withRecordID: recordId) { record, error in
 			if record != nil {
+                print("Username 2")
 				var _username = record!["name"]
 				if _username == nil || _username as! String == "" {
+                    print("Username 3")
 					_username = "bettrave" as CKRecordValue
 					record!.setObject(_username as CKRecordValue?, forKey: "name")
 					publicDB.save(record!) { record, error in
@@ -69,37 +73,48 @@ class StartViewController: UIViewController {
 				}
 				else {
 					username = _username
+                    print(username)
 				}
 			}
 		}
 	}
 	
 	private func fetchHighscoreAndRank(_ recordId: CKRecordID) {
+        print("Highscore 1")
 		
 		let query = CKQuery(recordType: "Highscore", predicate: NSPredicate(value: true))
 		query.sortDescriptors = [NSSortDescriptor(key: "score", ascending: false)]
 
+        print("Highscore 2")
 		let queryOperation = CKQueryOperation(query: query)
+        print("Highscore 3")
 		queryOperation.resultsLimit = 1
+        print("Highscore 4")
 		queryOperation.desiredKeys = ["score"]
+        print("Highscore 5")
 		queryOperation.recordFetchedBlock = { [unowned self] record in
+            print("Highscore 6")
 			currentHighscore = record["score"]
+            print("Highscore 7")
 			DispatchQueue.main.async {
+                print("Highscore 8")
 				self.loadingLabel.text = "Touch to start !"
 				self.tapToStartRecognizer.isEnabled = true
 			}
 		}
 		
 		privateDB.add(queryOperation)
+        print("Highscore 9")
 	}
 	
 	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("Prepare 1")
 		super.prepare(for: segue, sender: sender)
 		if segue.identifier == "errorSegue" {
 			let errorController = segue.destination as! ErrorViewController
 			self.loadingLabel.text = "Touch to start !"
 			self.tapToStartRecognizer.isEnabled = true
-			
+			print("Prepare 2")
 			errorController.error = error
 		}
 		//SystemSoundID.playFileNamed("0957")
